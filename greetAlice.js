96719eb31741c3d99e236733d6d094b4395c1900
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-03 09:59:51 +0600

    greet only Alice&Bob use if condation

---
+++ greetAlice.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter Alice & Bob only");
+if (name == "Alice") {
+  document.write("Wlecome Mr: " + name);
+} else if (name == "Bob") {
+  document.write("Wlecome Mr: " + name);
+} else {
+  document.write("Sorry Sir You are Not in the List !!");
+}
